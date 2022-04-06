2d6ff8797ac0313b7821f73ab07c75e8bfb2a587
Vitaly Posadnev <elemabor@gmail.com>
2022-04-06 19:33:19 +0300

    feat(app): remove daData interaction and change on curl requests

--- Parser.php
+++ Parser.php
@@ -1,6 +1,5 @@
 <?php
 
-use Dadata\DadataClient;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 
@@ -8,9 +7,30 @@ class Parser
 {
     private string $path;
 
+    private string $apiUrl = 'https://cleaner.dadata.ru/api/v1/clean';
+    private CurlHandle $handle;
+
     public function __construct(string $path)
     {
         $this->path = $path;
+
+        $creds = require_once 'config.php';
+        $token = $creds['token'];
+        $secret = $creds['secret'];
+        self::initDaData($token, $secret);
+    }
+
+    private function initDaData(string $token, string $secret)
+    {
+        $this->handle = curl_init();
+        curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->handle, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "Accept: application/json",
+            "Authorization: Token " . $token,
+            "X-Secret: " . $secret,
+        ));
+        curl_setopt($this->handle, CURLOPT_POST, 1);
     }
 
     public function getAddresses(): array
@@ -49,21 +69,32 @@ class Parser
     public function getGeoData(array $addresses): array
     {
         try {
-            $creds = require_once 'config.php';
-            $token = $creds['token'];
-            $secret = $creds['secret'];
-
-            $dadata = new DadataClient($token, $secret);
-            $result = [];
-
-            foreach ($addresses as $address) {
-                $response = $dadata->clean("address", $address);
-                $result[] = $response;
-            }
-
-            return $result;
+            $url = $this->apiUrl . "/address";
+            $fields = $addresses;
+            return $this->executeRequest($url, $fields);
         } catch (Exception $e) {
             return [];
         }
+    }
+
+    private function executeRequest($url, $fields)
+    {
+        curl_setopt($this->handle, CURLOPT_URL, $url);
+        curl_setopt($this->handle, CURLOPT_POST, 1);
+        curl_setopt($this->handle, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = $this->exec();
+        return json_decode($result, true);
+    }
+
+    private function exec()
+    {
+        $result = curl_exec($this->handle);
+        $info = curl_getinfo($this->handle);
+        if ($info['http_code'] == 429) {
+            throw new Exception('Too many requests');
+        } elseif ($info['http_code'] != 200) {
+            throw new Exception('Request failed with http code ' . $info['http_code'] . ': ' . $result);
+        }
+        return $result;
     }
 }
